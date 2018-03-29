e5843eb46d3f0d4131439c47631eca29acfb7750
Mazen Morgan <morgan.mazen42@gmail.com>
2018-03-29 14:39:31 +0200

    Lists For Custome Path

---
+++ TowerDefence/Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Block> path;
+
+	void Start ()
+    {
+        PrintAllWaypoints();
+
+    }
+	
+
+    private void PrintAllWaypoints()
+    {
+        foreach (Block waypoint in path)
+        {
+            print(waypoint);
+        }
+    }
+
+    void Update ()
+    {
+	}
+}
