8a875b675fe013284c54c4d88a76f1d9f333a35d
Mazen Morgan <morgan.mazen42@gmail.com>
2018-03-29 19:41:32 +0200

    The Dictionary Data Structure

--- TowerDefence/Assets/EnemyMovement.cs
+++ TowerDefence/Assets/EnemyMovement.cs
@@ -8,7 +8,6 @@ public class EnemyMovement : MonoBehaviour {
 
 	void Start ()
     {
-        StartCoroutine(PrintAllWaypoints());
 
     }
 	
---
+++ TowerDefence/Assets/Pathfinder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+
+    Dictionary<Vector3Int, Waypoint> grid = new Dictionary<Vector3Int, Waypoint>();
+   
+	void Start ()
+    {
+        LoadBlocks();
+	}
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            // Add them to the dictionary
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping Overlapping Block: " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+        print(grid.Count);
+    }
+
+    void Update ()
+    {
+		
+	}
+}
